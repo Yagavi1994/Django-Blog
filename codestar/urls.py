f96fef4f4fa5165ba7f867943f5c65016fed924c
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-07 21:44:23 +0000

    Create database.

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
+from blog.views import my_blog
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('', my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
