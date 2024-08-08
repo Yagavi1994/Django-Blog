d637031eed2c97392853ac55b4e687ca2e0eaa36
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-08 17:24:40 +0000

    Add genre to post model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
+GENRES = [
+    (0, "Adventure"), 
+    (1, "Romance"), 
+    (2, "Fantasy"), 
+    (3, "Science Fiction"), 
+    (4, "Mystery"), 
+    (5, "Horror"), 
+    (6, "Thriller"), 
+    (7, "Historical Fiction"), 
+    (8, "Comedy"), 
+    (9, "Drama"), 
+    (10, "Detective/Crime"), 
+    (11, "Western"), 
+    (12, "Biography/Autobiography"), 
+    (13, "Gothic"), 
+    (14, "Dystopian"), 
+    (15, "Epic"), 
+    (16, "Coming-of-Age"), 
+    (17, "Slice of Life"), 
+    (18, "Tragedy"), 
+    (19, "Mythology")
+]
 
 # Create your models here.
 
@@ -12,6 +34,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
+    genre = models.IntegerField(choices=GENRES, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
